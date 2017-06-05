677532605ef55f5b9956a46597812fae226b1c9d
Ivan <aivieien95@gmail.com>
2017-06-05 11:16:14 +0300

    Startup

---
+++ src/BlogApp/Scripts/app/app.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: "blog",
+    template:
+    `
+        <h1>Blog</h1>
+        <div>... the best is yet to come!</div>
+    `
+})
+
+export class AppComponent { }
